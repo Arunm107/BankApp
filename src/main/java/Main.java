6c4a88a02a0281e316f9a9cbc36d88ae07bf1331
Sharma <2058146@cognizant.com>
2023-10-17 23:12:42 +0530

    No change in master commit 1

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author 2058146 Arun Sharma
+ *
+ */
+public class Main {
+
+	public static void main (String[] args) {
+		
+		Account myaccount = new Account (1000); //Starting with $1000
+		
+		System.out.println("Initial balance: $" + myaccount.getBalance());
+		
+		myaccount.deposit(600); //Deposit $500
+		System.out.println("After Deposit: $" + myaccount.getBalance());
+
+		myaccount.withdraw(200); //Deposit $200
+		System.out.println("After withdrawal: $" + myaccount.getBalance());
+		
+//		myaccount.applyInterest();
+//		System.out.println("After applying interest: $" + myaccount.getBalance());
+
+		
+	}
+}
